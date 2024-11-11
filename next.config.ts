a2ee80ead4fc32db7cff0a390fe72b61e4e90873
Luca Vivona <lucavivona01@gmail.com>
2024-11-11 16:09:13 -0500

    remove api/docs path from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -27,20 +27,7 @@ const nextConfig: NextConfig = {
             ? "http://127.0.0.1:8000/:path*"
             : ":path*",
       },
-      {
-        source: "/api/docs",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/docs"
-            : "/docs",
-      },
-      {
-        source: "/openapi.json",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/openapi.json"
-            : "/openapi.json",
-      },
+
     ];
   },
 
