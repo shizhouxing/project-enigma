f06b134e8426566b07a5cfca22579726daaa43eb
Luca Vivona <lucavivona01@gmail.com>
2024-11-23 17:34:36 -0800

    add fontSize xxs in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ const config: Config = {
   ],
   theme: {
   	extend: {
+		fontSize: {
+			'xxs': '0.625rem', // Add a custom size (10px)
+		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
