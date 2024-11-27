7908b6151ec489d82f3ec47acffd887d5741ec17
Luca Vivona <lucavivona01@gmail.com>
2024-11-27 14:52:53 -0800

    remove Suspense from account layout

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -9,20 +9,12 @@ export default function AccountLayout({
 }>) {
   return (
     <>
-        <Suspense
-          fallback={
-            <Loading
-              fullScreen
-              className="flex items-center justify-center w-full h-screen text-center relative"
-            />
-          }
-        >
+
           <AppSidebar />
           <main className="flex-1 flex flex-col max-w-full ">
             <SideBarCloseButton />
             <div className="flex-1 pt-14 md:pt-0 ">{children}</div>
           </main>
-        </Suspense>
       </>
       );
 }
