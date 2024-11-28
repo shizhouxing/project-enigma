6334d9ee0d2e5004dc100ad1f420f98c7cdd5a57
Luca Vivona <lucavivona01@gmail.com>
2024-11-28 01:13:42 -0800

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -16,13 +16,14 @@ const nextConfig: NextConfig = {
     ],
   },
   rewrites: async () => {
-    const backendHost = process.env.NEXT_PUBLIC_BACKEND_HOST || 'http://127.0.0.1'; // Fallback to localhost if not set
+    const backendHost = process.env.NEXT_PUBLIC_BACKEND_HOST || 'http://127.0.0.1:8000'; // Fallback to localhost if not set
+    console.log(backendHost)
     return [
       {
         source: "/api/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? `${backendHost}:8000/:path*`
+            ? `${backendHost}/:path*`
             : `${backendHost}/:path*`,
       },
     ];
